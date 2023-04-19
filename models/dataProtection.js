934c35425d07fdec3bb080ce507d58abc67157c9
KannanG20 <kannan85006@gmail.com>
2023-04-19 17:09:32 +0530

    updated timestamps

--- models/dataProtection.js
+++ models/dataProtection.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const dataProtection = mongoose.Schema({
     content : String,
-})
+},
+{ timestamps: { createdAt: 'created_at' } }
+)
 
 module.exports = mongoose.model("DataProtection", dataProtection);
--- models/termsandconditions.js
+++ models/termsandconditions.js
@@ -2,6 +2,8 @@ const mongoose = require("mongoose");
 
 const terms_conditions = mongoose.Schema({
     content : String,
-})
+},
+{ timestamps: { createdAt: 'created_at' } }
+)
 
 module.exports = mongoose.model("TermsAndConditions", terms_conditions);
