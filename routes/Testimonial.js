f4b61a58e60643aec9fcfa07ec92e5e80601c6e1
KannanG20 <kannan85006@gmail.com>
2023-04-24 22:42:49 +0530

    added delete method

--- routes/Testimonial.js
+++ routes/Testimonial.js
@@ -7,5 +7,6 @@ router.post('/testimonial', testimonialController.POST_TESTIMONIAL);
 router.get('/testimonials', testimonialController.GET_TESTIMONIAL);
 router.get('/testimonial/:_id', testimonialController.GET_SINGLE_TESTIMONIAL)
 router.put('/testimonial/:_id', testimonialController.PUT_TESTIMONIAL)
+router.delete('/testimonial/:_id', testimonialController.DELETE_TESTIMONIAL)
 
 module.exports = router
