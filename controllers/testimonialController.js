e2687bc516fc5d9f660531b292da8e4ee8a7809b
KannanG20 <kannan85006@gmail.com>
2023-04-24 22:27:59 +0530

    .

--- controllers/testimonialController.js
+++ controllers/testimonialController.js
@@ -1,4 +1,5 @@
 const { validationResult } = require("express-validator");
+const mongoose = require("mongoose")
 
 const Testimonial = require("../models/testimonial");
 const customErrors = require("../utils/customError.js");
