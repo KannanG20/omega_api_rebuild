98c1929d473917038949d3948ec8f298b7f3e4a9
KannanG20 <kannan85006@gmail.com>
2023-05-05 09:04:17 +0530

    changed post method

--- controllers/Stats.js
+++ controllers/Stats.js
@@ -2,21 +2,38 @@ const Stats = require("../models/stats")
 
 exports.post_stats = async (req, res, next) => {
     try {
-        
-        const userStats = new Stats({
+      const newStats = new Stats({
+        stats: req.body
+      });
+      const data = await newStats.save();
+      res.status(200).json({
+        status: 'success',
+        results: data
+      });
+  
+      // check if 100 records exist in the database
+      const latestStats = await Stats.find().sort({ createdAt: -1 }).limit(100);
+      if (latestStats.length >= 100) {
+        // delete previous 100 records
+        await Stats.deleteMany({ createdAt: { $lte: latestStats[latestStats.length - 1].createdAt } });
+  
+        // insert new records
+        const newStatsArray = [];
+        const latestStatsIds = latestStats.map(stat => stat._id);
+        for (let i = 0; i < latestStatsIds.length; i++) {
+          newStatsArray.push({
+            _id: latestStatsIds[i],
             stats: req.body
-        }) 
-        const data = await userStats.save()
-        res.status(200).json({
-            status: 'success',
-            results: data
-        })
-
+          });
+        }
+        await Stats.insertMany(newStatsArray);
+      }
     } catch (error) {
-        console.log(error);
-        return next(error)
+      console.log(error);
+      return next(error);
     }
-}
+  }
+  
 
 exports.get_stats = async (req, res, next)=> {
     try {
