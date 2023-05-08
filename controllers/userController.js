eaf8bf2e24bd6a028ed0e3347fd1fe040ec02453
KannanG20 <kannan85006@gmail.com>
2023-05-08 12:56:13 +0530

    First commit

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,4 @@
 const { mongoose } = require("mongoose");
-const { validationResult } = require("express-validator");
 const bcrypt = require('bcrypt')
 
 const Whitelist = require('../models/whitelist')
