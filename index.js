bb78fddf93097f613e330bcfff2c9eb2aa1efe68
KannanG20 <kannan85006@gmail.com>
2023-05-08 13:05:50 +0530

    .

--- index.js
+++ index.js
@@ -24,11 +24,17 @@ const mongoDB = process.env.MONGO_DB;
 
 // Mongo DB connection 
 const mongooseConnect = async ()=> {
-    await mongoose.connect(mongoDB, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-}).then(console.log("connected to database")).catch((err)=> console.log(err));
+    try {
+        await mongoose.connect(mongoDB, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+        });
+        console.log("connected to database");
+    } catch (error) {
+        console.error(error);
+    }
 }
+
 mongooseConnect();
 
 // Start of backend server
